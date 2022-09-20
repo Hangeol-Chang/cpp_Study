ad5aedfb55ff1f712f02639d1a4fa9ef01d9a7a8
hihangeol <57909592+Hangeol-Chang@users.noreply.github.com>
2022-09-20 15:11:33 +0900

    [REFACTOR] CMAKE 빌드 설정

---
+++ build_test/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include "main.h"
+
+using namespace std;
+int main() {
+    int a = 1;
+    int b = 2;
+    
+    int c = add(a, b);
+    int d = sub(a, b); 
+
+    cout << c << " " << d << endl;
+}
